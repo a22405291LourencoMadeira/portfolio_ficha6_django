e9cec96e1d89243574287c904a0a0f602a26a433
LourençoMadeira22405291 <a22405291@alunos.ulht.pt>
2026-04-07 17:04:46 +0000

    Implementado modelo Licenciatura com admin configurado

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Licenciatura
 
-# Register your models here.
+@admin.register(Licenciatura)
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ['sigla', 'nome', 'duracao_anos']
+    search_fields = ['nome', 'sigla']

1ebe9c6aed1fa211f57c8ea9dac4485eca5892e0
LourençoMadeira22405291 <a22405291@alunos.ulht.pt>
2026-04-07 17:26:47 +0000

    Implementados todos os modelos e admin do portfolio e carregamento com alguns dados no Admin para testar

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,71 @@
 from django.contrib import admin
-from .models import Licenciatura
+from .models import (
+    Licenciatura, Docente, UnidadeCurricular,
+    Tecnologia, Projeto, TFC,
+    Competencia, Formacao, MakingOf
+)
+
 
 @admin.register(Licenciatura)
 class LicenciaturaAdmin(admin.ModelAdmin):
     list_display = ['sigla', 'nome', 'duracao_anos']
     search_fields = ['nome', 'sigla']
+
+
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'url_pagina']
+    search_fields = ['nome']
+
+
+@admin.register(UnidadeCurricular)
+class UnidadeCurricularAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'ano', 'semestre', 'ects', 'licenciatura']
+    list_filter = ['ano', 'semestre', 'licenciatura']
+    search_fields = ['nome']
+    filter_horizontal = ('docentes',)
+
+
+@admin.register(Tecnologia)
+class TecnologiaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'categoria', 'nivel_interesse']
+    list_filter = ['categoria', 'nivel_interesse']
+    search_fields = ['nome']
+
+
+@admin.register(Projeto)
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'ano', 'unidade_curricular']
+    list_filter = ['ano', 'unidade_curricular']
+    search_fields = ['nome']
+    filter_horizontal = ('tecnologias',)
+
+
+@admin.register(TFC)
+class TFCAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'area', 'ano', 'interesse']
+    list_filter = ['ano', 'interesse', 'area']
+    search_fields = ['titulo', 'area']
+    filter_horizontal = ('tecnologias',)
+
+
+@admin.register(Competencia)
+class CompetenciaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'nivel']
+    list_filter = ['nivel']
+    search_fields = ['nome']
+    filter_horizontal = ('tecnologias', 'projetos')
+
+
+@admin.register(Formacao)
+class FormacaoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'instituicao', 'data_inicio', 'data_fim']
+    list_filter = ['instituicao']
+    search_fields = ['nome', 'instituicao']
+
+
+@admin.register(MakingOf)
+class MakingOfAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'entidade_relacionada', 'data']
+    list_filter = ['entidade_relacionada']
+    search_fields = ['titulo', 'entidade_relacionada']
--- portfolio/models.py
+++ portfolio/models.py
@@ -9,3 +9,116 @@ class Licenciatura(models.Model):
 
     def __str__(self):
         return self.sigla
+
+
+class Docente(models.Model):
+    nome = models.CharField(max_length=200)
+    url_pagina = models.URLField()
+    foto = models.ImageField(upload_to='docentes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class UnidadeCurricular(models.Model):
+    nome = models.CharField(max_length=200)
+    ano = models.IntegerField()
+    semestre = models.IntegerField()
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='ucs/', blank=True, null=True)
+    ects = models.IntegerField()
+    licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE, related_name='ucs')
+    docentes = models.ManyToManyField(Docente, related_name='ucs', blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Tecnologia(models.Model):
+    CATEGORIAS = [
+        ('linguagem', 'Linguagem'),
+        ('framework', 'Framework'),
+        ('base_dados', 'Base de Dados'),
+        ('ferramenta', 'Ferramenta'),
+        ('outro', 'Outro'),
+    ]
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    logo = models.ImageField(upload_to='tecnologias/', blank=True, null=True)
+    url_website = models.URLField()
+    nivel_interesse = models.IntegerField()  # 1 a 5
+    categoria = models.CharField(max_length=50, choices=CATEGORIAS)
+
+    def __str__(self):
+        return self.nome
+
+
+class Projeto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    conceitos = models.TextField()
+    imagem = models.ImageField(upload_to='projetos/', blank=True, null=True)
+    video_demo = models.URLField(blank=True, null=True)
+    url_github = models.URLField()
+    ano = models.IntegerField()
+    unidade_curricular = models.ForeignKey(UnidadeCurricular, on_delete=models.CASCADE, related_name='projetos')
+    tecnologias = models.ManyToManyField(Tecnologia, related_name='projetos', blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class TFC(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    area = models.CharField(max_length=200)
+    instituicao = models.CharField(max_length=200)
+    ano = models.IntegerField()
+    interesse = models.BooleanField(default=False)
+    url = models.URLField(blank=True, null=True)
+    tecnologias = models.ManyToManyField(Tecnologia, related_name='tfcs', blank=True)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Competencia(models.Model):
+    NIVEIS = [
+        ('basico', 'Básico'),
+        ('intermedio', 'Intermédio'),
+        ('avancado', 'Avançado'),
+    ]
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    nivel = models.CharField(max_length=20, choices=NIVEIS)
+    tecnologias = models.ManyToManyField(Tecnologia, related_name='competencias', blank=True)
+    projetos = models.ManyToManyField(Projeto, related_name='competencias', blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Formacao(models.Model):
+    nome = models.CharField(max_length=200)
+    instituicao = models.CharField(max_length=200)
+    descricao = models.TextField()
+    data_inicio = models.DateField()
+    data_fim = models.DateField(blank=True, null=True)
+    certificado_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class MakingOf(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    decisoes = models.TextField()
+    erros_correcoes = models.TextField()
+    uso_ia = models.TextField(blank=True)
+    fotografia = models.ImageField(upload_to='makingof/', blank=True, null=True)
+    data = models.DateField()
+    entidade_relacionada = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo

65110bb05721448d38a23f7cb178057d44e60db4
LourençoMadeira22405291 <a22405291@alunos.ulht.pt>
2026-04-16 08:12:20 +0000

    Criados templates base.html e cursos.html para a app escola

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Curso
 
-# Create your views here.
+def cursos_view(request):
+    cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
+    return render(request, 'escola/cursos.html', {'cursos': cursos})

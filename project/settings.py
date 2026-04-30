5b724f1077ba601d4aba50bcf2423304e6e3a85e
LourençoMadeira22405291 <a22405291@alunos.ulht.pt>
2026-04-30 08:49:41 +0000

    Criada app accounts para autenticacao

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'portfolio',
     'escola',
     'markdownify.apps.MarkdownifyConfig', 
+    'accounts',
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",

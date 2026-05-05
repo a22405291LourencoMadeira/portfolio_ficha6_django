b8478957d04da10df066c7dcd3a30678fe913f60
LourençoMadeira22405291 <a22405291@alunos.ulht.pt>
2026-05-05 09:40:43 +0000

    Configurado admin para app artigos

--- artigos/admin.py
+++ artigos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Artigo, Like, Comentario
 
-# Register your models here.
+@admin.register(Artigo)
+class ArtigoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'data_criacao']
+    list_filter = ['autor', 'data_criacao']
+    search_fields = ['titulo', 'texto']
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ['artigo', 'autor', 'data']
+    list_filter = ['autor']
+
+admin.site.register(Like)

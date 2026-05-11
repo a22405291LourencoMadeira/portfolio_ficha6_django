ade25fc122befe45e6cb81564fdd4518a92a19e9
LourençoMadeira22405291 <a22405291@alunos.ulht.pt>
2026-05-11 17:43:46 +0000

    Removida configuracao local de media apos migracao para Cloudinary

--- project/settings.py
+++ project/settings.py
@@ -145,8 +145,8 @@ STATIC_URL = "static/"
 
 import os
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
